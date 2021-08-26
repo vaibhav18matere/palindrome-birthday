275814ad3204b9c7a5047566acf7af68f0814a7e
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-08-27 01:16:53 +0530

    get next date logic

--- app.js
+++ app.js
@@ -64,10 +64,66 @@ checkPalindromeForAllDateFormats = (date) => {
     return flag;
 }
 
+isLeapYear = (year) => {
+    if (year % 400 === 0) {
+        return true;
+    }
+    if (year % 100 === 0) {
+        return false;
+    }
+    if (year % 4 === 0) {
+        return true;
+    }
+    return false;
+}
+
+getNextDate = (date) => {
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (month === 2) {
+        if (isLeapYear(year)) {
+            if (day > 29) {
+                day = 1;
+                month++;
+            }
+        } else {
+            if (day > 28) {
+                day = 1;
+                month++;
+            }
+        }
+
+    } else {
+        if (day > daysInMonth[month - 1]) {
+            day = 1;
+            month++;
+        }
+    }
+    if (month > 12) {
+        month = 1;
+        year++;
+    }
+    return {
+        day: day,
+        month: month,
+        year: year
+    };
+}
+
+
+
+getNextPalindromeDate = (date) => {
+
+}
+
 var date = {
-    day: 13,
-    month: 9,
-    year: 2011
+    day: 28,
+    month: 2,
+    year: 2020
 };
 
-console.log(checkPalindromeForAllDateFormats(date));
+console.log(getNextDate(date));
